3d82bcf2554041d2916af71dbe427fbd89303fdc
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-27 14:25:52 +0200

    first commit

---
+++ resources/views/guest/templates/base.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <!-- Favicon -->
+  <link rel="shortcut icon" href="favicon.ico">
+
+  <title>@yield('title')</title>
+
+  <link rel="stylesheet" href="css/app.css">
+</head>
+<body>
+
+</body>
+</html>
